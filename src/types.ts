c5e7f0ad96042f73bd6d000519d303b2c86b1ad5
Gosha <for4ik45@mail.ru>
2023-10-07 08:39:17 +0300

    added eslint, prettier, stylelint

--- src/types.ts
+++ src/types.ts
@@ -1,87 +1,87 @@
 type Item = {
-    id: string
-    width: number
-    height: number
-    x: number
-    y: number
-    Copy(): void
-    Paste(): Item
-    Change(): Item
-    Delete(): void
+  id: string
+  width: number
+  height: number
+  x: number
+  y: number
+  Copy(): void
+  Paste(): Item
+  Change(): Item
+  Delete(): void
 }
 
 type Text = Item & {
-    fontFamily: string
-    fonSize: number
-    color: string
-    backgroundColor?: string
-    bold: boolean
-    italic: boolean
+  fontFamily: string
+  fonSize: number
+  color: string
+  backgroundColor?: string
+  bold: boolean
+  italic: boolean
 }
 
 type Image = Item & {
-    path: string
+  path: string
 }
 
 enum ShapeType {
-    Triangle,
-    Circle,
-    Square
+  Triangle,
+  Circle,
+  Square,
 }
 
 type Shape = Item & {
-    backgroundColor: string
-    borderColor: string
-    type: ShapeType
+  backgroundColor: string
+  borderColor: string
+  type: ShapeType
 }
 
 type Slide = {
-    slideNumber: number
-    background: Image | string
-    items: Item[]
-    CreateItem(): Item
-    ChangeBackground()
+  slideNumber: number
+  background: Image | string
+  items: Item[]
+  CreateItem(): Item
+  ChangeBackground()
 }
 
 type Presentation = {
-    name: string
-    slides: Slide[]
-    Create()
-    Rename()
-    Open()
-    CreateSlide(): Slide
-    Preview()
-    GetCurrentSlide(id: number): Slide
-    DeleteSlide(id: number): void
+  name: string
+  slides: Slide[]
+  Create()
+  Rename()
+  Open()
+  CreateSlide(): Slide
+  Preview()
+  GetCurrentSlide(id: number): Slide
+  DeleteSlide(id: number): void
 }
 
 type Export = {
-    presentation: Presentation
-    toPDF()
+  presentation: Presentation
+  toPDF()
 }
 
 type Action = {
-    presentationCopy: Presentation
-    actionNumber: number
+  presentationCopy: Presentation
+  actionNumber: number
 }
 
 type History = {
-    presentation: Presentation
-    actions: Action[]
-    PrevAction(): Presentation
-    NextAction(): Presentation
-    CreateAction(): Action
+  presentation: Presentation
+  actions: Action[]
+  PrevAction(): Presentation
+  NextAction(): Presentation
+  CreateAction(): Action
 }
 
 export {
-    Item,
-    Text,
-    Image,
-    ShapeType,
-    Shape,
-    Slide,
-    Export,
-    Action,
-    History,
-    Presentation
+  Item,
+  Text,
+  Image,
+  ShapeType,
+  Shape,
+  Slide,
+  Export,
+  Action,
+  History,
+  Presentation,
 }
